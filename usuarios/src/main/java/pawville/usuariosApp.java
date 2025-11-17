a86aecf55547e7161dcff173f561ba3b7f276ef6
Nicole <nc.2012.nicole@gmail.com>
2025-11-17 00:00:35 -0300

    primer commit

---
+++ usuarios/src/main/java/pawville/usuariosApp.java
@@ -0,0 +1,14 @@
+package pawville;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class usuariosApp
+{
+    public static void main( String[] args ) {
+        SpringApplication.run(usuariosApp.class, args);
+    }
+}
